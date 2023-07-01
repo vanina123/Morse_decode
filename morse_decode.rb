36ca8a777c940a3585b45f366e65b5fd88aee52d
vanina123 <nanavanina834@gmail>
2023-07-01 15:09:57 +0100

    morse decode

---
+++ morse_decode.rb
@@ -0,0 +1,23 @@
+def decode_char(morse_char)
+    morse_code = {
+      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+      '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3', '....-' => '4',
+      '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8',
+      '----.' => '9', '-----' => '0', ' ' => ' '
+    }
+  
+    morse_code[morse_char]
+  end
+  
+  def decode_word(morse_word)
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+  end
+  
+  def decode(message)
+    message.split('  ').map { |morse_word| decode_word(morse_word) }.join(' ')
+  end
+  
